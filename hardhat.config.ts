1a5bf33ddf3e3666cc18af80ef32b97419fcf73b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-11-19 09:16:19 +0800

    init commit

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.27",
+        settings: {
+            optimizer: {
+                enable: true,
+                runs: 1000,
+            },
+        },
+    },
+};
+
+export default config;
